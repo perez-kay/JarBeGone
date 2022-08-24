4885414f4cf7a2bfb7c05bdeb227ff76ec14f982
Kayla Perez <perezkay@uw.edu>
2022-08-24 16:11:32 -0700

    added comments

--- jarbegone/__init__.py
+++ jarbegone/__init__.py
@@ -1,3 +1,9 @@
+"""
+This script initializes the application instance, which handles all web server
+requests. It also initializes a cache instance used for data that is processed
+once upon the app's startup. 
+"""
+
 from flask import Flask
 from flask_caching import Cache
 from flask_bootstrap import Bootstrap
@@ -11,7 +17,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F2%J$LZGH*Uh'
 app.config.from_mapping(config)
 cache = Cache(app, config)
-
-
 bootstrap = Bootstrap(app)
+
 from jarbegone import routes
